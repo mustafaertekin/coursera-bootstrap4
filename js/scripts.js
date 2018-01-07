4e615f7a58c7dac0ff2628dc17e5b07e8baa9422
Mustafa Ertekin <mustafa014@gmail.com>
2018-01-07 23:04:46 +0100

    Assignment 4 full

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-button").click(function(){
-        if($("#carousel-button").children("span").hasClass("fa-pause")) {
+$(document).ready(function () {
+    $("#mycarousel").carousel({
+        interval: 2000
+    });
+    $("#carousel-button").click(function () {
+        if ($("#carousel-button").children("span").hasClass("fa-pause")) {
             $("#mycarousel").carousel('pause');
             $("#carousel-button").children("span").removeClass("fa-pause");
             $("#carousel-button").children("span").addClass("fa-play");
-        }
-        else if ($("#carousel-button").children("span").hasClass("fa-play")) {
+        } else if ($("#carousel-button").children("span").hasClass("fa-play")) {
             $("#mycarousel").carousel('cycle');
             $("#carousel-button").children("span").removeClass("fa-play");
             $("#carousel-button").children("span").addClass("fa-pause");
         }
     });
+
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+
+        $("#loginCancel").click(function () {
+            $("#loginModal").modal("hide")
+        });
+
+        $("#loginCancelX").click(function () {
+            $("#loginModal").modal("hide")
+        });
+    });
+    
+
+    $("#reserveButton").click(function () {
+        $("#reserveTableModal").modal("show");
+        
+        $("#reserveCancel").click(function () {
+            $("#reserveTableModal").modal("hide")
+        });
+
+        $("#reserveCancelX").click(function () {
+            $("#reserveTableModal").modal("hide")
+        })
+    });
+
 });
